6a1a22cbb3f3ce567e7c88511289e46aca13bacb
Chikusingh <40684539+Chikusingh@users.noreply.github.com>
2021-04-29 17:09:09 +0530

    Add files via upload

---
+++ config/Config.js
@@ -0,0 +1,10 @@
+let CONFIG = {};
+
+CONFIG.db_dialect = 'mysql';
+CONFIG.db_host = 'localhost';
+CONFIG.db_password = '';
+CONFIG.db_user = 'root';
+CONFIG.db_port = '';
+CONFIG.db_name = 'node_project';
+
+module.exports = CONFIG;
